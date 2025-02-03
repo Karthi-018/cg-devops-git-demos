9a5d1fcc3d8ab7912db4fa9f12c1001108082f2f
Karthi-018 <karthi.learning.2020@gmail.com>
2025-02-03 11:26:56 +0530

    Commiting the main branch

--- src/main/java/com/capgemini/training/git_demo/App.java
+++ src/main/java/com/capgemini/training/git_demo/App.java
@@ -13,5 +13,6 @@ public class App
         Logger log = Logger.getLogger(App.class.getName());
         log.info("Create Logger object and printing");
         log.info("writet the log to logfile" );
+        log.info("Creted a new Branch called test");
     }
 }
